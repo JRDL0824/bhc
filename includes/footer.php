641c75bce55be15093d389c1467c8e96785209c9
Michaellorenz6 <21-04399@g.batstate-u.edu.ph>
2023-05-04 09:20:31 +0800

    6-4

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
 <footer class="sticky-footer bg-white">
   <div class="container my-auto">
     <div class="copyright text-center my-auto">
-      <span>Copyright &copy; Health Center Reservation System 2021</span>
+      <span>Copyright &copy; Barangay Sambal Ilaya Health Center Appointment System 2023</span>
     </div>
   </div>
 </footer>
